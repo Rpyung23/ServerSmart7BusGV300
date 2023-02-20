eaef6842ef22304233a21fa19632e524a67c5a78
Rpyung23 <guaman1579@gmail.com>
2023-02-20 15:55:12 -0500

    update

--- pdo/cSocketCliente.js
+++ pdo/cSocketCliente.js
@@ -31,69 +31,71 @@ class CSocketCliente
         console.log("************************************************************************************")
         console.log(trama)
 
-        let tramaDividida = trama.split(',')
+        if(!trama.isEmpty()){
+            let tramaDividida = trama.split(',')
 
-        if (tramaDividida.length>0){
+            if (tramaDividida.length>0){
 
-            if(tramaDividida[0] == '+ACK:GTHBD')
-            {
-                ControllerVehiculo.registroControllerVehiculo(tramaDividida[2])
-            }else if(tramaDividida[0] == '+RESP:GTFRI')
-            {
-                console.log("FEcHA MONI : "+tramaDividida[13])
+                if(tramaDividida[0] == '+ACK:GTHBD')
+                {
+                    ControllerVehiculo.registroControllerVehiculo(tramaDividida[2])
+                }else if(tramaDividida[0] == '+RESP:GTFRI')
+                {
+                    console.log("FEcHA MONI : "+tramaDividida[13])
 
-                var fecha = tramaDividida[13]
-                /*var fechaInt = tramaDividida[13]
-                var fecha = fechaInt.toString()
-                var year  = fecha.substr(0,4)
-                var mm = fecha.substr(4,2)
-                var day = fecha.substr(6,2)
-                var hh = fecha.substr(8,2)
-                var min = fecha.substr(10,2)
-                var ss = fecha.substr(12,2)*/
-
-
-                const fechaObj = new Date(
-                    fecha.substr(0, 4), // Año
-                    fecha.substr(4, 2) - 1, // Mes (se resta 1 ya que los meses comienzan en 0)
-                    fecha.substr(6, 2), // Día
-                    fecha.substr(8, 2), // Hora
-                    fecha.substr(10, 2), // Minutos
-                    fecha.substr(12, 2) // Segundos
-                );
-                fechaObj.setHours(fechaObj.getHours() - 5);
-
-                const nuevaFecha = fechaObj.getFullYear().toString() +"-"+
-                    (fechaObj.getMonth() + 1).toString().padStart(2, "0") +"-"+
-                    fechaObj.getDate().toString().padStart(2, "0") + " " +
-                    fechaObj.getHours().toString().padStart(2, "0") +":"+
-                    fecha.substr(10, 2) +":"+fecha.substr(12, 2);
-
-                console.log(nuevaFecha); // Imprime la fecha y hora original menos 5 horas
+                    var fecha = tramaDividida[13]
+                    /*var fechaInt = tramaDividida[13]
+                    var fecha = fechaInt.toString()
+                    var year  = fecha.substr(0,4)
+                    var mm = fecha.substr(4,2)
+                    var day = fecha.substr(6,2)
+                    var hh = fecha.substr(8,2)
+                    var min = fecha.substr(10,2)
+                    var ss = fecha.substr(12,2)*/
 
 
+                    const fechaObj = new Date(
+                        fecha.substr(0, 4), // Año
+                        fecha.substr(4, 2) - 1, // Mes (se resta 1 ya que los meses comienzan en 0)
+                        fecha.substr(6, 2), // Día
+                        fecha.substr(8, 2), // Hora
+                        fecha.substr(10, 2), // Minutos
+                        fecha.substr(12, 2) // Segundos
+                    );
+                    fechaObj.setHours(fechaObj.getHours() - 5);
 
+                    const nuevaFecha = fechaObj.getFullYear().toString() +"-"+
+                        (fechaObj.getMonth() + 1).toString().padStart(2, "0") +"-"+
+                        fechaObj.getDate().toString().padStart(2, "0") + " " +
+                        fechaObj.getHours().toString().padStart(2, "0") +":"+
+                        fecha.substr(10, 2) +":"+fecha.substr(12, 2);
+
+                    console.log(nuevaFecha); // Imprime la fecha y hora original menos 5 horas
 
 
 
-                //fecha.splice(0,3)+"-"+fecha.splice(4,5)+"-"+fecha.splice(6,7)+" "+fecha.splice(8,9)+":"+fecha.splice(10,11)+":"+fecha.splice(12,13)
-                ControllerVehiculo.registroControllerMonitoreoVehiculo(tramaDividida[2],
-                    1,tramaDividida[8] == '' ? 0 : parseFloat(tramaDividida[8]),
-                    tramaDividida[13] == '' ? '1998-06-11 00:00:00' : nuevaFecha,
-                    tramaDividida[12] == '' ? 0 : parseFloat(tramaDividida[12]),
-                    tramaDividida[11] == '' ? 0 : parseFloat(tramaDividida[11]),
-                    tramaDividida[9] == '' ? 0 : parseInt(tramaDividida[9]))
 
-                try{
-                    ControllerVehiculo.registroControllerHistorialMonitoreoVehiculo(tramaDividida[2],
+
+
+                    //fecha.splice(0,3)+"-"+fecha.splice(4,5)+"-"+fecha.splice(6,7)+" "+fecha.splice(8,9)+":"+fecha.splice(10,11)+":"+fecha.splice(12,13)
+                    ControllerVehiculo.registroControllerMonitoreoVehiculo(tramaDividida[2],
                         1,tramaDividida[8] == '' ? 0 : parseFloat(tramaDividida[8]),
                         tramaDividida[13] == '' ? '1998-06-11 00:00:00' : nuevaFecha,
                         tramaDividida[12] == '' ? 0 : parseFloat(tramaDividida[12]),
                         tramaDividida[11] == '' ? 0 : parseFloat(tramaDividida[11]),
                         tramaDividida[9] == '' ? 0 : parseInt(tramaDividida[9]))
-                }catch (e) {
-                    console.log("HISTORIAL CALL CONTROLLER")
-                    console.log(e)
+
+                    try{
+                        ControllerVehiculo.registroControllerHistorialMonitoreoVehiculo(tramaDividida[2],
+                            1,tramaDividida[8] == '' ? 0 : parseFloat(tramaDividida[8]),
+                            tramaDividida[13] == '' ? '1998-06-11 00:00:00' : nuevaFecha,
+                            tramaDividida[12] == '' ? 0 : parseFloat(tramaDividida[12]),
+                            tramaDividida[11] == '' ? 0 : parseFloat(tramaDividida[11]),
+                            tramaDividida[9] == '' ? 0 : parseInt(tramaDividida[9]))
+                    }catch (e) {
+                        console.log("HISTORIAL CALL CONTROLLER")
+                        console.log(e)
+                    }
                 }
             }
         }
